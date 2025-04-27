239dd724d43c9729861ee65798ed2295e4ecd431
Wen162003 <wennch2003@gmail.com>
2025-04-27 00:14:18 -0500

    Se agrego la sección del hero

---
+++ src/view/HomeView/Sections/Herosection/HeroSection.vue
@@ -0,0 +1,22 @@
+<template>
+    <section class="flex flex-col md:flex-row gap-20">
+        <div class="w-full lg:w-1/2 space-y-8 p-6 md:p-20">
+            <h1 class=" font-extrabold leading-tight text-6xl text-white">
+                <Strong class="text-primary">Innovación</Strong> <br />
+                que impulsa tu crecimiento
+            </h1>
+            <p class="text-xl font-medium text-white">
+                Impulsamos tus procesos empresariales con soluciones seguras y
+                escalables.
+            </p>
+            <ButtonComponent text="¡Contactanos!" />
+        </div>
+        <div class="hidden lg:block">
+            <img src="../../../../assets/Img/ImagenInicial.png" alt="" />
+        </div>
+    </section>
+</template>
+
+<script setup>
+import ButtonComponent from "@/components/Atoms/Button/ButtonComponent.vue";
+</script>
