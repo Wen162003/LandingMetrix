fd1b946d6bbb0c912b4726ef32de2279af3c7106
wen162003 <wennch2003@gmail.com>
2025-04-26 19:40:43 -0500

    Configuracion inicial del proyecto

---
+++ src/components/Atoms/Button/ButtonComponent.vue
@@ -0,0 +1,33 @@
+<template>
+    <button
+      :class="[
+        'rounded-xl bg-primary text-black transition hover:bg-primary/90 cursor-pointer',
+        sizeClass
+      ]"
+    >
+      {{ text }}
+    </button>
+  </template>
+  
+  <script setup>
+  import { computed } from 'vue'
+  
+  const props = defineProps({
+    text: {
+      type: String,
+      required: true
+    },
+    size: {
+      type: String,
+      default: 'lg', 
+      validator: (value) => ['lg', 'sm'].includes(value)
+    }
+  })
+  
+  const sizeClass = computed(() => {
+    return props.size === 'lg'
+      ? 'text-[20px] py-3 px-4 font-medium'
+      : 'text-base py-2 px-4 font-medium'
+  })
+  </script>
+  
---
+++ src/layouts/HomeLayout.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="px-6 bg-backgraund h-screen">
+        <h1 class="text-3xl text-red-500">
+            layout
+        </h1>
+        <ButtonComponent text="Holaa" />
+        <InputComponent />
+    </div>
+</template>
+<script setup>
+import ButtonComponent from '../components/Atoms/Button/ButtonComponent.vue'
+import InputComponent from '../components/Atoms/Input/InputComponent.vue'
+
+</script>
