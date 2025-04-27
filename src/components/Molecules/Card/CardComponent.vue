45a45b9ac74cf6fc095106bcd1aef83a91f4321e
Wen162003 <wennch2003@gmail.com>
2025-04-27 00:19:09 -0500

    Se agrego la sección de Productos del metrix

---
+++ src/components/Molecules/Card/CardComponent.vue
@@ -0,0 +1,48 @@
+<template>
+    <div
+      class="flex w-[281px] flex-col items-start space-y-4 p-4 rounded-none bg-transparent"
+    >
+      <div class="w-full h-[206px] rounded-2xl overflow-hidden">
+        <img :src="img" alt="Imagen" class="w-full h-full object-cover" />
+      </div>
+  
+      <h2 class="text-3xl font-extrabold text-white">
+        {{ title }}
+      </h2>
+  
+      <p class="font-medium text-white">
+        {{ text }}
+      </p>
+  
+      <a
+        href="#"
+        class="font-medium text-white hover:text-primary cursor-pointer transition"
+      >
+        {{ linkText }}
+      </a>
+    </div>
+  </template>
+    
+    <script setup>
+  import { defineProps } from "vue";
+  
+  defineProps({
+    img: {
+      type: String,
+      default: "",
+    },
+    title: {
+      type: String,
+      default: "Lawyer",
+    },
+    text: {
+      type: String,
+      default:
+        "Plataforma para gestionar casos, audiencias y documentos legales de forma organizada y eficiente.",
+    },
+    linkText: {
+      type: String,
+      default: "Leer más",
+    },
+  });
+  </script>
---
+++ src/view/HomeView/Sections/ProductsSection/ProductsSection.vue
@@ -0,0 +1,29 @@
+<template>
+    <section class="px-6 md:px-20 py-16">
+        <div class="flex flex-col lg:flex-row justify-between py-8 space-y-6 md:space-y-0">
+            <h1 class="text-white font-extrabold text-4xl md:text-6xl leading-tight">
+                Casos de éxito <br> <strong class="text-primary">Innovadores</strong>
+            </h1>
+            <p class="text-white font-medium text-lg md:text-xl lg:w-1/3">
+                En <strong class="text-primary">Metrix</strong> , creamos soluciones
+                digitales únicas y a medida, impulsando el crecimiento de quienes
+                confían en nosotros.
+            </p>
+        </div>
+
+        <div class="flex justify-between gap-8 py-8 flex-wrap">
+            <Card :img="Lawyer" />
+            <Card :img="Cashing" title="Cashing"
+                text="Plataforma para gestionar y optimizar la recuperación de cartera empresarial" />
+            <Card :img="Data" title="Data"
+                text="software de gestión política facilita la planificación, análisis y seguimiento de campañas electorales para candidatos y partidos." />
+        </div>
+    </section>
+</template>
+
+<script setup>
+import Card from "@/components/Molecules/Card/CardComponent.vue";
+import Lawyer from "@/assets/Img/Lawyer.png";
+import Cashing from "@/assets/Img/Cashing.png";
+import Data from "@/assets/Img/Data.png";
+</script>
